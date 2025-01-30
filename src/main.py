68bf0e1ffd6a2c3e8ad5b528691f0008cdb5702e
Lennis Herburger <60411049+lennish-wex@users.noreply.github.com>
2025-01-30 11:23:23 -0800

    refactor for linting

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+"""Main function"""
 def run_table_catalog(session,
-                      target_database, 
+                      target_database,
                       catalog_database,
                       catalog_schema,
                       catalog_table,
@@ -11,7 +12,7 @@ def run_table_catalog(session,
                       update_comment,
                       n,
                       model):
-    
+
     """
     Catalogs data contained in Snowflake Database/Schema.
 
@@ -48,12 +49,14 @@ def run_table_catalog(session,
     import pandas as pd
     import snowflake.snowpark.functions as F
 
-    from tables import get_crawlable_tbls, get_unique_context, get_all_tables, add_records_to_catalog
+    from tables import get_crawlable_tbls, get_unique_context,\
+        get_all_tables, add_records_to_catalog
     from prompts import start_prompt
 
-    tables = get_crawlable_tbls(session, target_database, target_schema, 
+    tables = get_crawlable_tbls(session, target_database, target_schema,
                                 catalog_database, catalog_schema, catalog_table,
                                 replace_catalog)
+
     if include_tables:
         tables = list(set(tables).intersection(set(include_tables)))
     elif exclude_tables:
@@ -63,21 +66,27 @@ def run_table_catalog(session,
     #     tables = tables
 
     if tables:
-        context_db, context_schemas = get_unique_context(tables) # Database and set of Schemas to crawl
-        schema_df = get_all_tables(session, context_db, context_schemas) # Contains all tables in schema(s)
+         # Database and set of Schemas to crawl
+        context_db, context_schemas = get_unique_context(tables)
+         # Contains all tables in schema(s)
+        schema_df = get_all_tables(session, context_db, context_schemas)
+
         async_jobs = []
         for t in tables:
             current_schema = t.split('.')[1]
             prompt_args = { # Samples gathered during CATALOG_TABLE sproc
                 'tablename': t,
-                'table_columns': schema_df[schema_df.TABLENAME == t]['COLUMN_INFO'].to_numpy().item(),
-                'table_comment': schema_df[schema_df.TABLENAME == t]['TABLE_COMMENT'].to_numpy().item(),
+                'table_columns': schema_df[schema_df.TABLENAME == t]\
+                    ['COLUMN_INFO'].to_numpy().item(),
+                'table_comment': schema_df[schema_df.TABLENAME == t]\
+                    ['TABLE_COMMENT'].to_numpy().item(),
                 'schema_tables': schema_df[schema_df.TABLE_SCHEMA == current_schema]\
                                 .groupby('TABLE_SCHEMA')['TABLE_DDL']\
                                 .apply(list).to_numpy().item()[0]
             }
+
             # Samples passed later via double {{table_samples}}
-            prompt = start_prompt.format(**prompt_args).replace("'", "\\'") 
+            prompt = start_prompt.format(**prompt_args).replace("'", "\\'")
             query = f"""
             CALL {catalog_database}.{catalog_schema}.CATALOG_TABLE(
                                             tablename => '{t}',
@@ -90,35 +99,36 @@ def run_table_catalog(session,
             async_jobs.append(session.sql(query).collect_nowait())
 
         n = 20 # Give 20 seconds to finish first
+
         while not any(job.is_done() for job in async_jobs):
             time.sleep(n)
             n = 60 # Now check every minute
-            pass
 
         results = [json.loads(job.result()[0][0]) for job in async_jobs if job.is_done()]
-        # there is a bit of latency between when the query is done and when it is available in the query history
+        # there is a bit of latency between when the query is done and
+        # when it is available in the query history
         while len(results) != len(tables):
             results = [json.loads(job.result()[0][0]) for job in async_jobs if job.is_done()]
             time.sleep(60)
-            pass
-        
+
         df = session.create_dataframe(pd.DataFrame.from_records(results))\
                     .withColumn('EMBEDDINGS',
                                 F.call_udf('SNOWFLAKE.CORTEX.EMBED_TEXT_768',
                                            'e5-base-v2',
                                            F.col('DESCRIPTION')))\
                     .withColumn('CREATED_ON', F.current_timestamp())
-        
+
         add_records_to_catalog(session,
                                catalog_database,
                                catalog_schema,
                                catalog_table,
                                df,
                                replace_catalog)
-        
+
         # df.write.save_as_table(table_name = [catalog_database, catalog_schema, catalog_table],
         #                        mode = "append",
         #                        column_order = "name")
         return df
-    else:
-        return session.create_dataframe([['No new tables to crawl','']], schema=['TABLENAME', 'DESCRIPTION'])
+
+    return session.create_dataframe([['No new tables to crawl','']],
+                                     schema=['TABLENAME', 'DESCRIPTION'])
